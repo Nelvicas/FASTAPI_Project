2948c29f5c6d21b59aa0883a1fae191b28fff7a1
nelvi <nelvicas97@gmail.com>
2026-05-16 14:28:47 -0600

    Se añadio el archivo requirement.txt

--- project/database.py
+++ project/database.py
@@ -3,10 +3,10 @@ import hashlib
 import datetime
 from peewee import *
 
-from local_settings import USER_DATABASE
-from local_settings import PASSWORD_DATABASE
+from .local_settings import USER_DATABASE
+from .local_settings import PASSWORD_DATABASE
 
-database = MySQLDatabase('fastapi_project', 
+database = MySQLDatabase('fastapi_db', 
         user=USER_DATABASE,
         password=PASSWORD_DATABASE,
         host='localhost',
